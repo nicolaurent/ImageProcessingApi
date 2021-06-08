dd91ecf223869f83a128fc54ac667dfc59a727d9
Nico Laurent <nico.laurent1@gmail.com>
2021-06-08 14:07:59 +0800

    Remove supertest done callback

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -3,25 +3,22 @@ import app from '../index';
 
 const request = supertest(app);
 describe('Test endpoint responses', () => {
-    it('Home Page endpoint', async (done) => {
+    it('Home Page endpoint', async () => {
         const response = await request.get('/api');
         expect(response.status).toBe(200);
-        done();
     }
     
 )
 
-    it('images endpoint without filename', async (done) => {
+    it('images endpoint without filename', async () => {
         const response = await request.get('/api/images');
         expect(response.status).toBe(400);
-        done();
     }
 )
 
-    it('images endpoint with filename', async (done) => {
+    it('images endpoint with filename', async() => {
         const response = await request.get('/api/images?filename=encenadaport');
         expect(response.status).toBe(200);
-        done();
     }
 )
 });
